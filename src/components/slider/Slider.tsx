e96661a02421a1105e9715a045f8eaa5b6c2aa17
Shishka24 <shishkin241993@gmail.com>
2024-02-25 01:43:45 -0500

    4week/Slider/testimony

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,66 +1,47 @@
-import styled from "styled-components";
-import { FlexWrapper } from "../FlexWrapper";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./../../styles/slider.css";
+import {
+  SlideDivStyled,
+  StyledSlider,
+  StyledText,
+  StyledTitle,
+} from "./Slider_Styles";
 
-export const Slider = () => {
+type SlidePropsType = {
+  text: string;
+  userName: string;
+};
+
+const Slide = (props: SlidePropsType) => {
   return (
-    <StyledSlider>
-      <FlexWrapper>
-        <Slide>
-          <StyledText>
-            Text messages are used for personal, family, business, and social
-            purposes. Governmental and non-governmental organizations use text
-            messaging for communication between colleagues. In the 2010s, the
-            sending of short informal messages became an accepted part of many
-            cultures, as happened earlier with emailing.This makes texting a
-            quick and easy way to communicate with friends, family, and
-            colleagues, including in contexts where a call would be impolite or
-            inappropriate{" "}
-          </StyledText>
-          <StyledTitle>@Vladislav Sischin</StyledTitle>
-        </Slide>
-      </FlexWrapper>
-      <Pagination>
-        <span>.</span>
-        <span className={"active"}>.</span>
-        <span>.</span>
-      </Pagination>
-    </StyledSlider>
+    <SlideDivStyled>
+      <StyledText>{props.text}</StyledText>
+      <StyledTitle>@{props.userName}</StyledTitle>
+    </SlideDivStyled>
   );
 };
 
-const StyledSlider = styled.div`
-  max-width: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const Slide = styled.div`
-  /* text-align: center; */
-`;
-const StyledText = styled.p`
-  margin: 42px 0 42px;
-`;
-const StyledTitle = styled.span`
-  font-size: 16px;
-  font-weight: 600;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  margin: 22px 0 42px;
-  display: inline-block;
-`;
-const Pagination = styled.div`
-  span {
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    border-radius: 3cap;
-    background-color: #39775a;
-    cursor: pointer;
-    & + span {
-      margin-left: 5px;
+const items = [
+  <Slide
+    userName={"Ivan Belanger"}
+    text={
+      "Text messages are used for personal, family, business, and social urposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. In the 2010s, the sending of short informal messages became an accepted part of many cultures, as happened earlier with emailing.This makes texting a quick and easy way to communicate with friends, family, and colleagues, including in contexts where a call would be impolite or inappropriate"
     }
-    &.active {
-      background-color: #17df82;
-    }
-  }
-`;
+  />,
+  <Slide userName={"Ivan Belanger"} text={"blavla"} />,
+  <Slide userName={"Ivan Belanger"} text={"blavla"} />,
+  <Slide userName={"Ivan Belanger"} text={"blavla"} />,
+];
+
+export const Slider = () => (
+  <StyledSlider>
+    <AliceCarousel
+      mouseTracking
+      items={items}
+      // responsive={responsive}
+      // controlsStrategy="alternate"
+    />
+  </StyledSlider>
+);
---
+++ src/components/slider/Slider_Styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const StyledSlider = styled.div`
+  max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* border: 2px solid yellow; */
+`;
+export const SlideDivStyled = styled.div`
+  /* text-align: center; */
+`;
+export const StyledText = styled.p`
+  margin: 42px 0 42px;
+`;
+export const StyledTitle = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin: 22px 0 42px;
+  display: inline-block;
+`;
